fd1018b0127b9ee56d16c491fefa7c8ca30308e1
Patrick Hannan <patrick@hannan.com>
2020-09-09 23:52:46 -0400

    added the 2nd question function.

--- script.js
+++ script.js
@@ -107,3 +107,35 @@ function showQuestion1() {
         showQuestion2(); 
     }
 }
+
+function showQuestion2() {
+    questionEl.innerText = questions[currentQuestionsIndex].question;
+    button1.innerText = questions[currentQuestionsIndex].answers[0];
+    button2.innerText = questions[currentQuestionsIndex].answers[1];
+    button3.innerText = questions[currentQuestionsIndex].answers[2];
+    button4.innerText = questions[currentQuestionsIndex].answers[3];
+    button1.onclick = function () {
+        alert("Your answer is correct.");
+        currentQuestionsIndex++;
+        showQuestion3(); 
+        correctQuestions++
+    };
+    button2.onclick = function () {
+        alert("Your answer is incorrect.");
+        time -= 10;
+        currentQuestionsIndex++;
+        showQuestion3(); 
+    };
+    button3.onclick = function () {
+        alert("Your answer is incorrect.");
+        time -= 10;
+        currentQuestionsIndex++;
+        showQuestion3(); 
+    };
+    button4.onclick = function () {
+        alert("Your answer is incorrect.");
+        time -= 10;
+        currentQuestionsIndex++;
+        showQuestion3(); 
+    }
+}
